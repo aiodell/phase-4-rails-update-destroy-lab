08d1f778c5fbaa266e5836ac6f205dddf6c16de8
Andrianna <andrianna.odell@gmail.com>
2022-08-29 12:35:10 -0400

    commit tree 8407bbed0b0264b59116ef0b8945d5c6f3326231

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -18,9 +18,35 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  # PATCH /plants/:id
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant
+    else
+      plant_not_found
+    end
+  end
+
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      plant_not_found
+    end
+  end
+
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
+  end
+
+  def plant_not_found
+    render json: {error: "Plant not found" }, status: :not_found
   end
 end
